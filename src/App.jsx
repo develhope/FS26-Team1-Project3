d0fdfe60ce02adfc489663c7a5845f20ba0be79d
[Daiana] <[daiana.piastra2000@gmail.com]>
2024-06-07 19:35:30 +0200

    add mission

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-function App() {}
+import Mission from "./components/Mission";
+import './App.css'
+import Footer from "./components/Footer";
+
+function App() {
+    return(
+       <>
+       <Mission/>
+       <Footer/>
+       </>
+    )
+}
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+function Footer(params) {
+    return(
+        <div className="footer-container">
+           <div className="div-footer block-footer">
+            <img src="" alt="" />
+           </div>
+           <div className="div-form  block-footer">bla</div>
+        </div>
+    )
+}
+
+export default Footer
