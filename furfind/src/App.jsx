9068a482c2aacbeda0caa1bf29d3c104ce93af2e
valentinaaattardo <valentina.01.va@gmail.com>
2024-06-09 15:47:50 +0200

    first commit of card's section

---
+++ furfind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import SectionCard from './components/sectionCard'
+function App() {
+
+  return (
+   <SectionCard/>
+  )
+}
+
+export default App
---
+++ furfind/src/components/sectionCard.jsx
@@ -0,0 +1,18 @@
+import "./sectionCard.css"
+export default function sectionCard() {
+    return(
+<div className="book">
+<p>• 4 years</p>
+<p> • Medium size </p>
+<p>• Palermo,Italy</p>
+
+
+<div className="cover">
+    <img className="primaImmagine
+    " src="https://www.vitadacani.org/wp-content/uploads/2023/08/Nome_sitoVert2-28.jpg"
+    alt="foto lollo" />
+    <h2>Lollo</h2>
+
+</div>
+</div>)
+}
