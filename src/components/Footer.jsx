2a9440cea00873973bfc0bb5742784d3d7335037
[Daiana] <[daiana.piastra2000@gmail.com]>
2024-06-09 16:09:47 +0200

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,55 @@
+import React from 'react'
 import './Footer.css'
+import logo from '../assets/logo-senza-background.png'
+
+
 
 function Footer(params) {
     return(
-        <div className="footer-container">
-           <div className="div-footer block-footer">
-            <img src="" alt="" />
+        <div>
+          <div className="footer-container">
+             <div className="div-footer block-footer">
+                <div>
+
+                </div>
+                <div className='footer-logo'>
+                   <img src={logo} alt="logo" /> 
+                </div>
+                 <div className='footer-list'>
+                    <ul>
+                   <li>Home</li>
+                   <li>About us</li>
+                   <li>Service</li>
+                   <li>Cart</li>
+                 </ul>
+                 </div>
+                 
+             </div>
+             <div className="div-form  block-footer">
+                <div className='form-title'>
+                    <h2>Contattaci</h2>
+                </div>
+                <form className='form-footer'>
+                    <div className='fullname-footer '>
+                    <input placeholder="Fullname" className="input" name="text" type="text"/>
+
+                       <input type="email" name='email' placeholder='Email' className="input"/> 
+                    </div>
+                    <div>
+                      <input type="text" name='message' className="input message" placeholder='Message' />   
+                    </div>
+                    <div className="container">
+                      <button type="submit" className="button">Submit</button>
+                    </div>
+                </form>
+
+             </div>
+          </div>  
+           <div>
+            
            </div>
-           <div className="div-form  block-footer">bla</div>
         </div>
+        
     )
 }
 
--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,26 +1,54 @@
 import React from "react"
 import './Mission.css'
-function Mission(params) {
+import paw from '../assets/paw-2-svgrepo-com.svg'
+
+
+function Mission() {
     return(
         <div className="mission-container">
-            <div className="mission-text">
-                <h2 className="mission-title">La nostra missione</h2>
-                <p className="misssion-parag">
-                 La nostra missione è quella di salvaguardare la vita di cani e gatti abbandonati, promuovendo l'adozione responsabile e consapevole, offrendo un rifugio temporaneo, sicuro e amorevole per gli animali in attesa di trovare una famiglia permanente.
-                </p>
-
-                
-                <h3 className="mission-htre">I nostri Valori Fondamentali:</h3>
-                <p><b>Amore e Rispetto per gli Animali: </b> Ogni animale merita una vita piena di amore, rispetto e dignità.</p>
-                <p> <b>Trasparenza: </b>Forniamo informazioni chiare e oneste su ogni animale e sul processo di adozione.</p>
-                <p><b>Responsabilità: </b>Promuoviamo l'adozione responsabile e ci assicuriamo che i nostri adottanti siano preparati per le responsabilità che comporta prendersi cura di un animale domestico.</p>
-                <p> <b>Comunità: </b>Crediamo nella forza della comunità e nel lavorare insieme per il bene degli animali.</p>
             
-                  <b> <p className="mission-thanks">Insieme possiamo fare la differenza nella vita di molti amici a quattro zampe, dando loro una seconda possibilità e una famiglia amorevole.</p> </b> 
+            <div className="mission-text">
+                <div className="mission-zampetta">
+                 <img src={paw} alt="zampetta"/>
+                </div>
+                
+                <div>
+                    <div>
+                      <h2 className="mission-title">La nostra missione</h2>    
+                    </div>
+                    <div>
+                        <p className="misssion-parag">
+                      La nostra missione è quella di salvaguardare la vita di cani e gatti abbandonati, <br/>promuovendo l'adozione responsabile e consapevole,  offrendo un rifugio temporaneo, sicuro e amorevole <br />per gli animali in attesa di trovare una famiglia permanente.
+                      </p>
+                </div>
+                  
+                </div>
 
-                   <p><b>Grazie per il vostro sostegno e per essere parte della nostra missione!</b></p>  
+                <div>
+                    <h3 className="mission-htre">I nostri Valori Fondamentali:</h3>
+                 <p><b>Amore e Rispetto per gli Animali: </b> Ogni animale merita una vita piena di amore, rispetto e dignità.</p>
+
+                 <p> <b>Trasparenza: </b>Forniamo informazioni chiare e oneste su ogni animale e sul processo di adozione.</p>
+
+                 <p><b>Responsabilità: </b>Promuoviamo l'adozione responsabile e ci assicuriamo che i nostri adottanti siano<br />  preparati per leresponsabilità che comporta prendersi cura di un animale domestico.</p>
                 
+                 <p> <b>Comunità: </b>Crediamo nella forza della comunità e nel lavorare insieme per il bene degli animali.</p>
+                 </div>
+                 
+
+                 <div>
+                   <b> <p className="mission-thanks">Insieme possiamo fare la differenza nella vita di molti amici a quattro zampe,<br /> dando loro una seconda possibilità e una famiglia amorevole.</p> </b> 
+                </div>                   
+
                 
+                <div>
+                    <p><b>Grazie per il vostro sostegno e per essere parte della nostra missione!</b></p>  
+                </div>
+            
+                <div className="mission-zampetta2 ">
+                 <img src={paw} alt="zampetta"/>
+                </div>
+
             </div>
         </div>
     )
