ec008c31e16f92f4d59b0813a9cccb85044ae0c4
[Daiana] <[daiana.piastra2000@gmail.com]>
2024-06-17 11:29:27 +0200

    add fix  mission/footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,53 +1,49 @@
-import React from 'react'
 import './Footer.css'
-import logo from '../assets/logo-senza-background.png'
+import logo from '../assets/logo furfind white vertical.svg'
+import twitter from '../assets/twitter-icon.svg'
+import instagram from '../assets/instagram-icon.svg'
+import facebook from '../assets/facebook-icon.svg'
+import youtube from '../assets/youtube-icon.svg'
 
 
 
-function Footer(params) {
+function Footer() {
     return(
         <div>
           <div className="footer-container">
              <div className="div-footer block-footer">
-                <div>
-
-                </div>
-                <div className='footer-logo'>
-                   <img src={logo} alt="logo" /> 
-                </div>
+                  <div className='logoFooter'>
+                     <img src={logo} alt="vertical logo" />
+                  </div>
                  <div className='footer-list'>
                     <ul>
-                   <li>Home</li>
-                   <li>About us</li>
-                   <li>Service</li>
-                   <li>Cart</li>
-                 </ul>
+                      <li>Home</li>
+                      <li>Chi siamo</li>
+                      <li>Servizi</li>
+                      <li>Carrello</li>
+                      <li>Politica sulla privacy</li>
+                   </ul>
                  </div>
-                 
-             </div>
-             <div className="div-form  block-footer">
-                <div className='form-title'>
-                    <h2>Contattaci</h2>
-                </div>
-                <form className='form-footer'>
-                    <div className='fullname-footer '>
-                    <input placeholder="Fullname" className="input" name="text" type="text"/>
-
-                       <input type="email" name='email' placeholder='Email' className="input"/> 
-                    </div>
-                    <div>
-                      <input type="text" name='message' className="input message" placeholder='Message' />   
-                    </div>
-                    <div className="container">
-                    <button type="button" className="cta-button">Submit</button>
-                    </div>
-                </form>
-
-             </div>
+                 <div className='social'> 
+                   <div className='media'>
+                     <img src={twitter} alt="twitter icon" />
+                   </div>
+                   <div className='media'>
+                     <img src={instagram} alt="instagram icon" />
+                   </div>
+                   <div className='media'>
+                     <img src={facebook} alt="facebook icon" />
+                   </div>
+                   <div className='media'>
+                     <img src={youtube} alt="youtube icon" />
+                   </div>
+                 </div> 
+                 <div className='copyright'>
+                    <p>Le informazioni riguardanti gli animali adottabili presentati su Furfind sono fornite dalle persone che ne hanno assunto la cura. Furfind non effettua alcun controllo o verifica sull&#39;accuratezza di queste informazioni. Tutte queste informazioni devono essere verificate con il rifugio e/o la persona interessata e in particolare che l&#39;animale sia ancora disponibile per l&#39;adozione.Lo stato di salute e il comportamento degli animali presentati, trovati o adottati tramite Furfind sono sotto la piena responsabilità dell&#39;organismo che li propone per l&#39;adozione o della persona che li adotta. La responsabilità dell&#39;uso di Furfind è esclusivamente e integralmente dell&#39;utente che esonera Furfind da ogni responsabilità riguardante l&#39;adozione di un animale presentato su questo sito.</p>
+                    <p>©2024 Furfind</p>
+                 </div>
+             </div>   
           </div>  
-           <div>
-            
-           </div>
         </div>
         
     )
--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,4 +1,3 @@
-import React from "react"
 import './Mission.css'
 import paw from '../assets/paw-2-svgrepo-com.svg'
 
@@ -8,9 +7,6 @@ function Mission() {
         <div className="mission-container">
             
             <div className="mission-text">
-                <div className="mission-zampetta">
-                 <img src={paw} alt="zampetta"/>
-                </div>
                 
                 <div>
                     <div>
@@ -18,26 +14,26 @@ function Mission() {
                     </div>
                     <div>
                         <p className="misssion-parag">
-                      La nostra missione è quella di salvaguardare la vita di cani e gatti abbandonati, <br/>promuovendo l'adozione responsabile e consapevole,  offrendo un rifugio temporaneo, sicuro e amorevole <br />per gli animali in attesa di trovare una famiglia permanente.
+                      La nostra missione è quella di salvaguardare la vita di cani e gatti abbandonati, promuovendo l&#39;adozione responsabile e consapevole,  offrendo un rifugio temporaneo, sicuro e amorevole per gli animali in attesa di trovare una famiglia permanente.
                       </p>
                 </div>
                   
                 </div>
 
                 <div>
-                    <h3 className="mission-htre">I nostri Valori Fondamentali:</h3>
-                 <p><b>Amore e Rispetto per gli Animali: </b> Ogni animale merita una vita piena di amore, rispetto e dignità.</p>
+                    <h3 className="mission-htre">I nostri Valori Fondamentali:</h3> <br />
+                 <p><b>Amore e Rispetto per gli Animali: </b> Ogni animale merita una<br /> vita  piena di amore, rispetto e dignità.</p>
 
-                 <p> <b>Trasparenza: </b>Forniamo informazioni chiare e oneste su ogni animale e sul processo di adozione.</p>
+                 <p> <b>Trasparenza:</b>Forniamo informazioni chiare e oneste su ogni <br /> animale e sul processo di adozione.</p>
 
-                 <p><b>Responsabilità: </b>Promuoviamo l'adozione responsabile e ci assicuriamo che i nostri adottanti siano<br />  preparati per leresponsabilità che comporta prendersi cura di un animale domestico.</p>
+                 <p><b>Responsabilità:  </b>Promuoviamo l&#39;adozione responsabile e ci assicuriamo che i nostri adottanti siano  preparati per le responsabilità che comporta prendersi cura di un animale domestico.</p>
                 
-                 <p> <b>Comunità: </b>Crediamo nella forza della comunità e nel lavorare insieme per il bene degli animali.</p>
+                 <p> <b>Comunità: </b>Crediamo nella forza della comunità e nel <br /> lavorare insieme per il bene degli animali.</p>
                  </div>
                  
 
                  <div>
-                   <b> <p className="mission-thanks">Insieme possiamo fare la differenza nella vita di molti amici a quattro zampe,<br /> dando loro una seconda possibilità e una famiglia amorevole.</p> </b> 
+                   <b> <p className="mission-thanks">Insieme possiamo fare la differenza nella vita di molti amici a quattro zampe, dando loro una seconda possibilità e una famiglia amorevole.</p> </b> 
                 </div>                   
 
                 
@@ -46,6 +42,9 @@ function Mission() {
                 </div>
             
                 <div className="mission-zampetta2 ">
+                 <img src={paw} alt="zampetta"/>
+                 <img src={paw} alt="zampetta"/>
+                 <img src={paw} alt="zampetta"/>
                  <img src={paw} alt="zampetta"/>
                 </div>
 
