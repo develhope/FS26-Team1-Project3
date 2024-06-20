c73d09af82ccbf3ac0e4be4627bd742c5eb9f0ff
Fabio Mariantoni <spreadoibaf@gmail.com>
2024-06-20 13:07:18 +0200

    add iscrivitiform component and Route to navbar to be fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
-import "../css/Hero.scss";
-import heroImage from "../assets/hero.jpg";
-import Navbar from "./Navbar";
-import HeroCaption from "./HeroCaption";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from 'react';
+import '../css/Hero.scss';
+import heroImage from '../assets/hero.jpg';
+import Navbar from './Navbar';
+import HeroCaption from './HeroCaption';
 
 export default function Hero() {
   return (
