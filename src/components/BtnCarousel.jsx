349f03b3bfe5139a4bab257bb1754e58403af922
valentinaaattardo <valentina.01.va@gmail.com>
2024-06-28 17:36:34 +0200

    fixed section card with button

---
+++ src/components/BtnCarousel.jsx
@@ -0,0 +1,59 @@
+import SectionCard from "./sectionCard";
+import "../css/BtnCarousel.css";
+import { useState } from "react";
+import pets from "../pets/pets";
+
+function BtnCarousel() {
+  const [startIndex, setStartIndex] = useState(0);
+  const cardsPerPage = 4;
+
+  const nextCard = () => {
+    if (startIndex + cardsPerPage < pets.length) {
+      setStartIndex((prevStartIndex) => prevStartIndex + 1);
+    } else {
+      setStartIndex(0);
+    }
+  };
+
+  const prevCard = () => {
+    if (startIndex === 0) {
+      setStartIndex(pets.length - cardsPerPage);
+    } else {
+      setStartIndex((prevStartIndex) => prevStartIndex - 1);
+    }
+  };
+
+  const visibleCards = pets.slice(startIndex, startIndex + cardsPerPage);
+
+  return (
+    <div className="app">
+      <button className="carousel-button" onClick={prevCard}>
+        &#9664;
+      </button>
+      <div className="containerCarousel">
+        {visibleCards.map(({ image, name, age, city, description, razza, nameImage }, index) => (
+          <div
+            key={index}
+            className="carousel-card"
+          >
+            <SectionCard
+              image={image}
+              name={name}
+              age={age}
+              city={city}
+              description={description}
+              razza={razza}
+              nameImage={nameImage}
+            />
+          </div>
+        ))}
+      </div>
+      <button className="carousel-button" onClick={nextCard}>
+        &#9654;
+      </button>
+    </div>
+  );
+}
+
+export default BtnCarousel;
+
