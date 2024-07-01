087671cc7af2b9d7f3a4daccc7824dfa00fb4e2d
angelog5 <angelo.gavini3@gmail.com>
2024-07-01 11:26:16 +0200

    forms updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import LoginForm from "./components/LoginForm";
-import SignUpForm from './components/SignUpForm.jsx';
-import NewAnnounce from './components/NewAnnounce.jsx';
-
-
+import SignUpForm from "./components/SignUpForm.jsx";
+import NewAnnounce from "./components/NewAnnounce.jsx";
 
 function App() {
   return (
-   <Router>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/iscriviti" element={<SignUpForm />} />
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/annuncio" element={<NewAnnounce />} />
-    </Routes>
-   </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/iscriviti" element={<SignUpForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/annuncio" element={<NewAnnounce />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,26 +1,48 @@
-import '../css/form.css';
+import "../css/form.css";
+import FormBackground from "./FormBackground";
 
 export default function LoginForm() {
   return (
     <div className="form-container">
-    <div className="form_area">
+      <FormBackground />
+      <div className="form_area">
         <p className="title">LOG IN</p>
         <form action="">
-            <div className="form_group">
-                <label className="sub_title" htmlFor="name">Dati di accesso</label>
-                <input placeholder="Inserisci nome o e-mail" className="form_style" type="text"/>
-            </div>
-            <div className="form_group">
-                <label className="sub_title" htmlFor="password">Password</label>
-                <input placeholder="inserisci la tua password" id="password" className="form_style" type="password"/>
-            </div>
-            <div className='btn-area'>
-                <button className="btn">ACCEDI</button>
-                <p>Non hai un Account? <a className="link" href="">Iscriviti qua!</a></p><a className="link" href="">
-            </a></div><a className="link" href="">
-        
-    </a></form></div><a className="link" href="">
-</a></div>
-  )
- 
+          <div className="form_group">
+            <label className="sub_title" htmlFor="name">
+              Dati di accesso
+            </label>
+            <input
+              placeholder="Inserisci nome o e-mail"
+              className="form_style"
+              type="text"
+            />
+          </div>
+          <div className="form_group">
+            <label className="sub_title" htmlFor="password">
+              Password
+            </label>
+            <input
+              placeholder="inserisci la tua password"
+              id="password"
+              className="form_style"
+              type="password"
+            />
+          </div>
+          <div className="btn-area">
+            <button className="btn">ACCEDI</button>
+            <p>
+              Non hai un Account?{" "}
+              <a className="link" href="./iscriviti">
+                Iscriviti qua!
+              </a>
+            </p>
+            <a className="link" href=""></a>
+          </div>
+          <a className="link" href=""></a>
+        </form>
+      </div>
+      <a className="link" href=""></a>
+    </div>
+  );
 }
