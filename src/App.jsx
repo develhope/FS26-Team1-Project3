5d3cb37f8c49e52547b6035b3e2c678d08d7da65
[Daiana] <[daiana.piastra2000@gmail.com]>
2024-07-03 16:25:34 +0200

    fix about component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./components/HomePage";
 import LoginForm from "./components/LoginForm";
 import SignUpForm from "./components/SignUpForm.jsx";
 import NewAnnounce from "./components/NewAnnounce.jsx";
+import About from "./components/About.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/iscriviti" element={<SignUpForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/annuncio" element={<NewAnnounce />} />
+        <Route path="/about" element={<About/>}/>
       </Routes>
     </Router>
   );
