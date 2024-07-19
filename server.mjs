f5cc8afbef5a4d5392aeee0bfcddc87e1102f539
Fabio Mariantoni <spreadoibaf@gmail.com>
2024-07-19 13:05:33 +0200

    fix server

--- server.mjs
+++ server.mjs
@@ -104,5 +104,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port http://localhost:3000`);
 });
